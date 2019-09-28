05c4b852b72f7aa2e7b0f0268af302f63bc5aa85
Seunghoon Oh <stdsalmon@gmail.com>
2019-09-28 22:55:32 +0900

    Init project

---
+++ app/src/main/java/com/tistory/deque/stickyscrollviewtest/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.tistory.deque.stickyscrollviewtest
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        main_scroll_view.run {
+            header = header_view
+            stickListener = { _ ->
+                Log.d("LOGGER_TAG", "stickListener")
+            }
+            freeListener = { _ ->
+                Log.d("LOGGER_TAG", "freeListener")
+            }
+        }
+
+    }
+}
